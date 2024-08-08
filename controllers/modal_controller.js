2795d0925fda82668c6527927bd07f35b13f6819
Nolan J Tait <nolanjtait@gmail.com>
2024-08-08 12:10:05 -0700

    Adds #close and #handleFormSubmit on modal controller

--- controllers/modal_controller.js
+++ controllers/modal_controller.js
@@ -3,7 +3,17 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = [ "modal" ]
 
+  close() {
+    this.modalTarget.close()
+  }
+
   open() {
     this.modalTarget.showModal()
+  }
+
+  handleFormSubmit(event) {
+    if (event.detail.success) {
+      this.close()
+    }
   }
 }
