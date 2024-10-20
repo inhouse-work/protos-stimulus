8350975545f84f693c254901a076cc489beecd49
Nolan J Tait <nolanjtait@gmail.com>
2024-10-19 17:32:23 -0700

    Adds better handling of open and close states for modal controller

--- controllers/modal_controller.js
+++ controllers/modal_controller.js
@@ -2,12 +2,23 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   static targets = [ "modal" ]
+  static values = {
+    open: { type: Boolean, default: false }
+  }
+
+  connect() {
+    if (this.openValue) {
+      this.open()
+    }
+  }
 
   close() {
+    this.modalTarget.classList.remove("modal-open")
     this.modalTarget.close()
   }
 
   open() {
+    this.modalTarget.classList.add("modal-open")
     this.modalTarget.showModal()
   }
 
